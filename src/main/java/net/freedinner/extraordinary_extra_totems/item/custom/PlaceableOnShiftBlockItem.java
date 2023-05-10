e9f8d1d62e539d640e190782e96e924573304435
Phill77715 <freedinn3r@gmail.com>
2023-05-10 16:27:44 +0200

    Updated version, added tooltips

--- src/main/java/net/freedinner/extraordinary_extra_totems/item/custom/PlaceableOnShiftBlockItem.java
+++ src/main/java/net/freedinner/extraordinary_extra_totems/item/custom/PlaceableOnShiftBlockItem.java
@@ -2,8 +2,16 @@ package net.freedinner.extraordinary_extra_totems.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PlaceableOnShiftBlockItem extends BlockItem {
     public PlaceableOnShiftBlockItem(Block block, Settings settings) {
@@ -13,5 +21,11 @@ public class PlaceableOnShiftBlockItem extends BlockItem {
     @Override
     protected boolean canPlace(ItemPlacementContext context, BlockState state) {
         return context.getPlayer().isSneaking() && super.canPlace(context, state);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable("item.extraordinary_extra_totems.shift_item.tooltip").formatted(Formatting.GRAY));
     }
 }
